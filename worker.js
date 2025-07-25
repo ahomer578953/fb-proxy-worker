123af009bd348d510e974fa3a586d5923915d685
ahomer578953 <ahomer578953@gmail.com>
2025-07-25 06:31:54 +0200

    worker.js

---
+++ worker.js
@@ -0,0 +1,32 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const target = 'https://www.peddygist.com'
+
+  const url = new URL(request.url)
+  const targetUrl = target + url.pathname + url.search
+
+  const modifiedHeaders = new Headers(request.headers)
+  modifiedHeaders.set('Host', 'www.peddygist.com')
+  modifiedHeaders.set('User-Agent', 'Mozilla/5.0 (Linux; Android 14; SM-A245F Build/UP1A.231005.007; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/133.0.6943.122 Mobile Safari/537.36 [FBAN/InternetOrgApp;FBAV/166.0.0.0.169;]')
+  modifiedHeaders.set('X-iorg-bsid', '@Magdi2051')
+  modifiedHeaders.set('Proxy-Connection', 'keep-alive')
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: request.method,
+      headers: modifiedHeaders,
+      body: request.method === 'GET' || request.method === 'HEAD' ? null : request.body,
+      redirect: 'follow'
+    })
+
+    return new Response(response.body, {
+      status: response.status,
+      headers: response.headers
+    })
+  } catch (e) {
+    return new Response('Error connecting to www.peddygist.com', { status: 502 })
+  }
+}
